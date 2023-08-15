04b8977bf276875371f017d8bfedf6c24e60f1a9
merdi elongo <merdielongo9@gmail.com>
2023-08-15 12:37:16 +0100

    integration inertiaJs

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,65 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+        </template>
+
+        <div class="py-12">
+
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="grid grid-cols-4 gap-4">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900">
+                            Stores <br>
+                            <b>10k</b>
+                        </div>
+                    </div>
+
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900">
+                            Commandes  <br>
+                            <b>31k</b>
+                        </div>
+                    </div>
+
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900">
+                            Articles  <br>
+                            <b>50k</b>
+                        </div>
+                    </div>
+
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900">
+                            Utilisateurs  <br>
+                            <b>120k</b>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+                    <div class="grid grid-cols-4 gap-4">
+                        <a href="#" class="bg-gray-900 text-white p-6 rounded-lg text-center uppercase text-[13px]">Ajouter un store</a>
+                        <a href="#" class="bg-gray-900 text-white p-6 rounded-lg text-center uppercase text-[13px]">Créer un utilisateur</a>
+                        <a href="#" class="bg-gray-900 text-white p-6 rounded-lg text-center uppercase text-[13px]">Voir la liste de licenses</a>
+                        <a href="#" class="bg-gray-900 text-white p-6 rounded-lg text-center uppercase text-[13px]">Lancer une publiciter</a>
+                        <a href="#" class="bg-gray-900 text-white p-6 rounded-lg text-center uppercase text-[13px]">Voir le compte de paiements</a>
+                        <a href="#" class="bg-gray-900 text-white p-6 rounded-lg text-center uppercase text-[13px]">Voir la liste de demandes</a>
+                        <a href="#" class="bg-gray-900 text-white p-6 rounded-lg text-center uppercase text-[13px]">Afficher les livraisons</a>
+                        <a href="#" class="bg-gray-900 text-white p-6 rounded-lg text-center uppercase text-[13px]">Configuration zandoshop</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </AuthenticatedLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Livewire\Counter;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,20 +16,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/counter', Counter::class);
-//Route::get('/template', function () {
-//    return view('layouts.template');
-//});
-Route::get('/', [\App\Http\Controllers\PageController::class, 'index'])->name('pages.index');
-Route::get('/about', [\App\Http\Controllers\PageController::class, 'about'])->name('pages.about');
-Route::get('/contact', [\App\Http\Controllers\PageController::class, 'contact'])->name('pages.contact');
-Route::get('/shop', [\App\Http\Controllers\PageController::class, 'shop'])->name('pages.shop');
-Route::get('/product', [\App\Http\Controllers\PageController::class, 'product'])->name('pages.product');
+require __DIR__.'/auth.php';
+
+Route::group(['middleware' => 'auth', 'prefix' => 'bko'], function () {
+    
+});
